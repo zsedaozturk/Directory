890e9d299b43a27fddcb80bb3009f7fff532a9f7
zsedaozturk <96082447+zsedaozturk@users.noreply.github.com>
2022-07-17 00:23:38 +0300

    UserManager.cs, Presentation Layer, Controllers are added.

---
+++ Directory.Contact.Business/Managers/UserManager.cs
@@ -0,0 +1,74 @@
+﻿using Directory.ContactService.Business.DTOs;
+using Directory.ContactService.DataAccess.CSContext;
+using Directory.ContactService.DataAccess.Entities;
+using Directory.ContactService.DataAccess.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Directory.ContactService.Business.Managers
+{
+    public class UserManager : IUserManager
+    {
+        UnitOfWork uow = new UnitOfWork(new ContactServiceContext());
+
+
+
+        public User ConvertToUser(UserDto userDto)
+        {
+            User user = new User();
+            user.ID = userDto.UserID;
+            user.Name = userDto.Name;
+            user.Surname = userDto.Surname;
+            user.CompanyName = userDto.CompanyName;
+            return user;
+        }
+        public void AddUser(UserDto userDto)
+        {
+            User user = ConvertToUser(userDto);
+            uow.UserRepository.AddUser(user);
+            uow.Save();
+        }
+        public void DeleteUser(Guid userId)
+        {
+            User user = uow.UserRepository.getUserByUserId(userId);
+            uow.UserRepository.DeleteUser(user);
+            uow.Save();
+        }
+
+        public List<UserDto> getAllUsersContacts()
+        {
+            List<UserDto> userDtos = uow.UserRepository.getAllUsers().Select(x => new UserDto
+            {
+                UserID = x.ID,
+                Name = x.Name,
+                Surname = x.Surname,
+                CompanyName = x.CompanyName
+
+            }).ToList();
+
+            var usersIDs = userDtos.Select(x => x.UserID).ToList();
+
+            var contactsDtos = uow.ContactRepository.getAllContacts().Where(x => usersIDs.Contains(x.UserID)).Select(y => new ContactDto
+            {
+                ID = y.ID,
+                Email = y.Email,
+                PhoneNumber = y.PhoneNumber,
+                Address = y.Address,
+                UserID = y.UserID
+
+            }).ToList();
+
+
+            foreach (var item in userDtos)
+            {
+                item.contactDtos = contactsDtos.Where(x => x.UserID == item.UserID).ToList();
+
+            }
+
+            return userDtos;
+        }
+
+    }
+}
---
+++ Directory.ContactService.PresentationLayer/Controllers/ContactServiceController.cs
@@ -0,0 +1,82 @@
+﻿using Directory.ContactService.Business.DTOs;
+using Directory.ContactService.Business.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Directory.ContactService.PresentationLayer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserServiceController : Controller
+    {
+
+        IUserManager userManager;
+        IContactManager contactManager;
+        public UserServiceController(IUserManager _userManager, IContactManager _contactManager)
+        {
+            userManager = _userManager;
+            contactManager = _contactManager;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void AddUser(UserDto userDto)
+        {
+            Guid guid = Guid.NewGuid();
+            userDto.UserID = guid;
+            userManager.AddUser(userDto);
+        }
+        [HttpDelete]
+        public void DeleteUser(UserDto userDto)
+        {
+            contactManager.DeleteContactByUserId(userDto.UserID);
+            userManager.DeleteUser(userDto.UserID);
+
+        }
+
+        public void DeleteContact(ContactDto contactDto)
+        {
+            contactManager.DeleteContact(contactDto.ID);
+        }
+
+        [HttpPost]
+        public void AddContact(ContactDto contactDto)
+        {
+            contactManager.AddContact(contactDto);
+        }
+
+        [HttpGet]
+        public List<UserDto> getAllUsersContacts()
+        {
+            List<UserDto> listusers = userManager.getAllUsersContacts();
+            return listusers;
+        }
+        [HttpPost]
+        public List<ContactDto> GetContact(ContactDto contactDto)
+        {
+            return contactManager.getAllContactsById(contactDto.UserID);
+        }
+
+        [HttpPost]
+        public ReportDto CreateReport(ContactDto contactDto)
+        {
+            List<int> counts = contactManager.PersonPhoneCount(contactDto.Address);
+            ReportDto reportDto = new ReportDto();
+            reportDto.Location = contactDto.Address;
+            reportDto.UserCount = counts[0];
+            reportDto.PhoneCount = counts[1];
+
+            return reportDto;
+        }
+
+
+    }
+}
