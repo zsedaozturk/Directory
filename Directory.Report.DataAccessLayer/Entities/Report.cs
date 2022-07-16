c5040548318e0107153ee0dfe0553e52f674e9e0
zsedaozturk <96082447+zsedaozturk@users.noreply.github.com>
2022-07-16 18:14:28 +0300

    Contact and Report Entities Added

---
+++ Directory.Report.DataAccessLayer/Entities/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Directory.Report.DataAccess.Entities
+{
+    public class Report
+    {
+        public Guid ID { get; set; }
+        public string ReportName { get; set; }
+        public string Location { get; set; }
+        public int PersonCount { get; set; }
+        public int PhoneCount { get; set; }
+        public DateTime RequestedDate { get; set; }
+
+        public ReportEnum status { get; set; }
+    }
+}
