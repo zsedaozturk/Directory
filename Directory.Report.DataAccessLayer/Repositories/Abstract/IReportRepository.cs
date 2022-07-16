0fcbed1810df5002cd1910e0340e821daab7d7e7
zsedaozturk <96082447+zsedaozturk@users.noreply.github.com>
2022-07-17 02:09:13 +0300

    Report Service Data Access Layer adds and Business Layer is created.

---
+++ Directory.Report.DataAccessLayer/Repositories/Abstract/IReportRepository.cs
@@ -0,0 +1,15 @@
+﻿using Directory.ReportService.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Directory.ReportService.DataAccess.Repositories.Abstract
+{
+    public interface IReportRepository
+    {
+        void AddReport(Report report);
+        List<Report> GetReports();
+        Report GetReportById(Guid id);
+        void DeleteReport(Report report);
+    }
+}
---
+++ Directory.ReportService.Business/Managers/ReportManager.cs
@@ -0,0 +1,85 @@
+﻿using Directory.ReportService.Business.DTOs;
+using Directory.ReportService.DataAccess.Entities;
+using Directory.ReportService.DataAccess.Repositories.Concrete;
+using Directory.ReportService.DataAccess.RSContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Directory.ReportService.Business.Managers
+{
+    public class ReportManager : IReportManager
+    {
+        ReportRepository reportRepository = new ReportRepository(new ReportServiceContext());
+        public Report ConvertToReport(ReportDto reportDto)
+        {
+            Report report = new Report();
+            report.ID = reportDto.ID;
+            report.ReportName = reportDto.ReportName;
+            report.Location = reportDto.Location;
+            report.PersonCount = reportDto.PersonCount;
+            report.PhoneCount = reportDto.PhoneCount;
+            report.RequestedDate = reportDto.RequestedDate;
+            report.status = reportDto.status;
+
+            return report;
+        }
+
+
+        public ReportDto ConvertToDto(Report report)
+        {
+            ReportDto reportDto = new ReportDto();
+            reportDto.ID = report.ID;
+            reportDto.ReportName = report.ReportName;
+            reportDto.PersonCount = report.PersonCount;
+            reportDto.PhoneCount = report.PhoneCount;
+            reportDto.RequestedDate = report.RequestedDate;
+            reportDto.status = report.status;
+
+            return reportDto;
+        }
+
+
+        public void AddReport(ReportDto reportDto)
+        {
+            Report report = ConvertToReport(reportDto);
+            reportRepository.AddReport(report);
+        }
+
+        public List<ReportDto> GetReports()
+        {
+            List<Report> reports = reportRepository.GetReports();
+
+            List<ReportDto> reportDtos = new List<ReportDto>();
+
+            foreach (Report r in reports)
+            {
+                ReportDto reportDto = new ReportDto();
+                reportDto.ID = r.ID;
+                reportDto.Location = r.Location;
+                reportDto.ReportName = r.ReportName;
+                reportDto.PersonCount = r.PersonCount;
+                reportDto.PhoneCount = r.PhoneCount;
+                reportDto.CreatedDate = r.RequestedDate.ToShortDateString() + "-" + r.RequestedDate.ToShortTimeString();
+                reportDto.status = r.status;
+
+                reportDtos.Add(reportDto);
+
+            }
+
+
+            return reportDtos;
+        }
+
+
+
+        public void DeleteReport(Guid id)
+        {
+            Report report = reportRepository.GetReportById(id);
+            reportRepository.DeleteReport(report);
+        }
+
+
+
+    }
+}
